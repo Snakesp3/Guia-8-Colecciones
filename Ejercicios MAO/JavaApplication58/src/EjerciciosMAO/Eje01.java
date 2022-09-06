bed5bd67d81e38472908597664f839ac038c0fcc
Sebastian G <resgirolminipcparts2014@gmail.com>
2022-09-06 18:52:30 -0300

    primer Ejercicio MAO

---
+++ Ejercicios MAO/JavaApplication58/src/EjerciciosMAO/Eje01.java
@@ -0,0 +1,22 @@
+
+package EjerciciosMAO;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.TreeSet;
+
+
+public class Eje01 {
+
+    
+    public static void main(String[] args) {
+      
+        
+        ArrayList<Integer> listado = new ArrayList();
+        TreeSet<String> nombre = new TreeSet();
+        HashMap<Integer,String> personas = new HashMap();
+
+        
+    }
+    
+}
