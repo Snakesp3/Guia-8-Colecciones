e5be5180abb3ccaf6b19a291ec09c2bc804e3e00
Sebastian G <resgirolminipcparts2014@gmail.com>
2022-09-08 18:58:03 -0300

    Actualizando

---
+++ Ejercicios MAO/Guia 8 Colecciones F03/src/Entidad/Entidad_alumno.java
@@ -0,0 +1,70 @@
+
+package Entidad;
+
+/*Crear una clase llamada Alumno que mantenga información sobre las notas de distintos
+alumnos. La clase Alumno tendrá como atributos, su nombre y una lista de tipo Integer
+con sus 3 notas.
+En el servicio de Alumno deberemos tener un bucle que crea un objeto Alumno. Se pide
+toda la información al usuario y ese Alumno se guarda en una lista de tipo Alumno y se le
+pregunta al usuario si quiere crear otro Alumno o no.
+Después de ese bucle tendremos el siguiente método en el servicio de Alumno:
+Método notaFinal(): El usuario ingresa el nombre del alumno que quiere calcular su nota
+final y se lo busca en la lista de Alumnos. Si está en la lista, se llama al método. Dentro
+del método se usará la lista notas para calcular el promedio final de alumno. Siendo este
+promedio final, devuelto por el método y mostrado en el main.*/
+public class Entidad_alumno {
+    
+    public String nombres;
+    public Integer nota1;
+    public Integer nota2;
+    public Integer nota3;
+
+    public Entidad_alumno() {
+    }
+
+    public Entidad_alumno(String nombres, Integer nota1, Integer nota2, Integer nota3) {
+        this.nombres = nombres;
+        this.nota1 = nota1;
+        this.nota2 = nota2;
+        this.nota3 = nota3;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public Integer getNota1() {
+        return nota1;
+    }
+
+    public void setNota1(Integer nota1) {
+        this.nota1 = nota1;
+    }
+
+    public Integer getNota2() {
+        return nota2;
+    }
+
+    public void setNota2(Integer nota2) {
+        this.nota2 = nota2;
+    }
+
+    public Integer getNota3() {
+        return nota3;
+    }
+
+    public void setNota3(Integer nota3) {
+        this.nota3 = nota3;
+    }
+    
+    
+    
+    
+    
+    
+    
+}
