465a3cded408c41920ca0dd5e187459dc0dd0123
Sebastian G <resgirolminipcparts2014@gmail.com>
2022-09-07 21:53:58 -0300

    Actualizando

--- Ejercicios MAO/Guia 8 Colecciones F02/src/Main/MainPerros.java
+++ Ejercicios MAO/Guia 8 Colecciones F02/src/Main/MainPerros.java
@@ -1,21 +1,59 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Main;
 
-/**
- *
- * @author Snakesp2
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Scanner;
+
+/*Diseñar un programa que lea y guarde razas de perros en un ArrayList de tipo String. El
+programa pedirá una raza de perro en un bucle, el mismo se guardará en la lista y
+después se le preguntará al usuario si quiere guardar otro perro o si quiere salir. Si decide
+salir, se mostrará todos los perros guardados en el ArrayList. 
  */
 public class MainPerros {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
+       
+        ArrayList<String> Dog = new ArrayList();
+        Scanner Leer = new Scanner(System.in);
+        
+        String R = "S"; 
+        
+        do {
+            
+            System.out.println("Ingrese la raza del perro");
+            Dog.add(Leer.next());
+            
+            System.out.println("Desea ingresar otra raza de perro S/N");
+            R = Leer.next();   
+            
+        } while (R.equalsIgnoreCase("S"));   
+        
+        System.out.println(Dog.toString());
+        
+        System.out.println("Ingrese la raza del perro");
+        String Raza = Leer.next();
+        
+        Iterator <String> nombre = Dog.iterator();
+        
+        boolean found=false;
+        while(nombre.hasNext()){
+            
+            if (Raza.equals(nombre.next())) {
+                
+                nombre.remove();
+                found = true;
+            } 
+        }
+        if (!found) {
+            System.out.println("La raza no se encontro");
+        } 
+        System.out.println(Dog.toString());
+       
+    
     
 }
+   }
+    
+
